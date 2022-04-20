34f90bc15e8fe71be1132291f37030ad8837be6d
Victor <victor1996d@gmail.com>
2022-04-20 13:08:48 +0200

    Updated folder distribution

--- django-blog/blog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Category, Post, Comment, Profile
-
-# Register your models here.  
-
-admin.site.register(Post)
-admin.site.register(Category)
-admin.site.register(Comment)
-admin.site.register(Profile)
